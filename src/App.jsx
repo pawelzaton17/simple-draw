2f0be8eb38adb5d0a39f3fede533f50b990d434d
pawel.zaton17 <pawzatkontakt@gmail.com>
2025-04-15 22:30:42 +0200

    added copyrights section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SimpleDraw from "./components/SimpleDraw";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <SimpleDraw />
+      <Footer />
     </div>
   );
 }
