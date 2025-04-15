1594f7dd03b2229fa4629cadfe35462a5c049ec3
pawel.zaton17 <pawzatkontakt@gmail.com>
2025-04-15 19:28:04 +0200

    fix/formated files

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,6 +1,28 @@
-export const predefinedStores = ["Leclerc", "Kaufland", "Netto", "Biedronka", "Lidl"];
+export const predefinedStores = [
+  "Leclerc",
+  "Kaufland",
+  "Netto",
+  "Biedronka",
+  "Lidl",
+];
 
-export const commonProducts = ["Napój gazowany", "Napój niegazowany", "Sok", "Żelki", "Cukierki twarde", "Cukierki miekkie", "Batoniki z wafelkiem", "Batoniki zwykłe", "Czekolada", "Chipsy", "Chrupki", "Słone przekąski", "Ciasteczka", "Cały dzien jedne kolor", "Słodkie przekąski"];
+export const commonProducts = [
+  "Napój gazowany",
+  "Napój niegazowany",
+  "Sok",
+  "Żelki",
+  "Cukierki twarde",
+  "Cukierki miekkie",
+  "Batoniki z wafelkiem",
+  "Batoniki zwykłe",
+  "Czekolada",
+  "Chipsy",
+  "Chrupki",
+  "Słone przekąski",
+  "Ciasteczka",
+  "Cały dzien jedne kolor",
+  "Słodkie przekąski",
+];
 
 export const cheatDayOptions = ["Pizza", "Kebab", "Lody"];
 
